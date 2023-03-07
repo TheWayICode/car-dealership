254275865b286eade62031c1c19d381f79c5ab8d
nazraf3 <nazraf@gmail.com>
2023-03-06 19:09:54 -0500

    Initial setup, Sales Models and encoders complete

---
+++ sales/api/sales_rest/encoders.py
@@ -0,0 +1,51 @@
+from common.json import ModelEncoder
+
+from .models import AutomobileVO, Salesperson, Customer, SalesRecord
+
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "id",
+        "color",
+        "vin",
+        "import_href"
+    ]
+
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "id",
+        "name",
+        "address",
+        "phone",
+    ]
+
+
+class SalespersonEncoder(ModelEncoder):
+    model = Salesperson
+    properties = [
+        "id",
+        "name",
+        "employee_number",
+    ]
+
+
+class SalesRecordEncoder(ModelEncoder):
+    model = SalesRecord
+    properties = [
+        "id",
+        "price",
+        "salesperson",
+        "customer",
+        "automobile",
+    ]
+    encoders = {
+        "salesperson": SalespersonEncoder(),
+        "customer": CustomerEncoder(),
+        "automobile": AutomobileVOEncoder(),
+    }
+
+    def get_extra_data(self, o):
+        return {"id": o.id}
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    color = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+    import_href = models.CharField(max_length=200, unique=True, null=True)
+
+    def __str__(self):
+        return self.vin
+
+class Salesperson(models.Model):
+    name = models.CharField(max_length=200)
+    employee_number = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class SalesRecord(models.Model):
+    price = models.IntegerField(default=0)
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="sales_record",
+        on_delete=models.PROTECT,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales_record",
+        on_delete=models.PROTECT,
+    )
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales_record",
+        on_delete=models.DO_NOTHING,
+    )
